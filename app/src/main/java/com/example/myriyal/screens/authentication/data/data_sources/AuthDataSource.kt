ad1acf1656a5456ac5713ad439ac87a26298651e
Ahad <ahadsaad45@gmail.com>
2025-04-15 11:51:40 +0300

    MR-64- Clean Architecture for Authentication (Sign-up) with applying DI

--- app/src/main/java/com/example/myriyal/screens/authentication/data/data_sources/AuthDataSource.kt
+++ app/src/main/java/com/example/myriyal/screens/authentication/data/data_sources/AuthDataSource.kt
@@ -3,8 +3,9 @@ package com.example.myriyal.screens.authentication.data.data_sources
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.tasks.await
+import javax.inject.Inject
 
-class AuthDataSource (private val auth: FirebaseAuth) {
+class AuthDataSource @Inject constructor (private val auth: FirebaseAuth) {
 
     suspend fun signUpWithEmailPassword(email: String, password: String): FirebaseUser? {
         val result = auth.createUserWithEmailAndPassword(email, password).await()
--- app/src/main/java/com/example/myriyal/screens/authentication/data/repositories_imp/AuthRepoImp.kt
+++ app/src/main/java/com/example/myriyal/screens/authentication/data/repositories_imp/AuthRepoImp.kt
@@ -3,8 +3,9 @@ package com.example.myriyal.screens.authentication.data.repositories_imp
 import com.example.myriyal.screens.authentication.data.data_sources.AuthDataSource
 import com.example.myriyal.screens.authentication.domain.repository.AuthRepo
 import com.google.firebase.auth.FirebaseUser
+import javax.inject.Inject
 
-class AuthRepoImp (private val dataSource: AuthDataSource): AuthRepo {
+class AuthRepoImp @Inject constructor (private val dataSource: AuthDataSource): AuthRepo {
     override suspend fun signUpWithEmailPassword(
         username: String,
         email: String,
