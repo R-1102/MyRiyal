685546e7230abb705c942ccb2a77cbaf74360117
Renad <renadalhazmi29@gmail.com>
2025-04-18 15:42:40 +0300

    MR-73 Enhance Record UI

--- app/src/main/java/com/example/myriyal/ui/theme/Color.kt
+++ app/src/main/java/com/example/myriyal/ui/theme/Color.kt
@@ -2,32 +2,46 @@ package com.example.myriyal.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+//Light Theme Colors
 val PrimaryGreen = Color(0xFF005430)
 val SecondaryGreen = Color(0xFF209261)
-val LightSurface = Color(0xFFF8F8F8)
+val LightBackground = Color(0xFFF8F8F8)
 val onSurface = Color(0xFF000000)
-val Gray = Color(0xFF87948E)
+val SecondaryContainer = Color(0xFFFFFFFF)
+val lightGray = Color(0xFFECECEC)
 
+//Dark Theme Colors
 val DarkGreen = Color(0xFF082B27)
 val GreenGrey40 = Color(0xFF335446)
 val DarkSurface = Color(0xFFC5C5C5)
+val DarkSecondaryContainer = Color(0xFF596C5D)
+val lightGreen = Color(0xFF8FA292)
 
+//Riyal Colors
 val Expense = Color(0xFFBE3F3F)
 val Income = Color(0xFF005E1F)
-
 
 //GradientButton Colors
 val firstGreen = Color(0xC9005732)
 val secondGreen = Color(0xEB004D2C)
 val thirdGreen = Color(0xFFF00532F)
 
+//GradientDarkButton Colors
 val firstDarkGreen = Color(0xC9082B27)
 val secondDarkGreen = Color(0xEB082B27)
 val thirdDarkGreen = Color(0xFFF082B27)
 
-val firstDarkAdd = Color(0xC9949494)
-val secondDarkAdd= Color(0xEB757575)
+//GradientDarkFloatingButton Colors
+val firstDarkAdd = Color(0xC9737373)
+val secondDarkAdd= Color(0xEB4F4F4F)
 
 //Filter Buttons Colors
 val Beige = Color(0xFF90754D)
+val DarkBeige = Color(0xFFC2AF9D)
+
+//Basic Colors
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val Red = Color(0xFFB22626)
+
 
