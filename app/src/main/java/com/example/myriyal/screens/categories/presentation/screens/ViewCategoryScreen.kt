542b082791800ec98aff7a42f85136ee195be803
Renad <renadalhazmi29@gmail.com>
2025-04-20 21:30:43 +0300

    MR-89 Implement the UI for Filter Category

--- app/src/main/java/com/example/myriyal/screens/categories/presentation/screens/ViewCategoryScreen.kt
+++ app/src/main/java/com/example/myriyal/screens/categories/presentation/screens/ViewCategoryScreen.kt
@@ -1,20 +1,29 @@
 package com.example.myriyal.screens.categories.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.integerResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.myriyal.R
@@ -28,8 +37,8 @@ fun ViewCategoryScreen(categoryViewModel: CategoryViewModel = hiltViewModel()) {
         categoryViewModel.seedPredefinedCategories()
     }
 
-    val viewModel: CategoryViewModel = hiltViewModel()
-    val categories by viewModel.categories.collectAsState()
+    val categories by categoryViewModel.categories.collectAsState()
+    var isDropdownExpanded by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -40,19 +49,47 @@ fun ViewCategoryScreen(categoryViewModel: CategoryViewModel = hiltViewModel()) {
             )
     ) {
         Column(
-            Modifier
-                .fillMaxWidth()
+            Modifier.fillMaxWidth()
         ) {
+            //filter and search Row
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = integerResource(id = R.integer.extraSmallSpace).dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+                Box {
+                    IconButton(
+                        onClick = { isDropdownExpanded = true },
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.filter),
+                            contentDescription = "Filter",
+                            tint = MaterialTheme.colorScheme.onPrimary,
+                        )
+                    }
+
+                    CategoryFilterDropdown(
+                        expanded = isDropdownExpanded,
+                        onDismiss = { isDropdownExpanded = false }
+                    )
+                }
+            }
             // Category list section
             if (categories.isEmpty()) {
                 Text("No categories found.")
             } else {
                 LazyColumn {
-                    items(items = categories, key = { it.categoryId }) { category ->
+                    items(
+                        items = categories,
+                        key = { it.categoryId }
+                    )
+                    { category ->
                         CategoryItemCard(
                             category = category,
                             onEdit = { TODO() },
-                            onSoftDelete = { viewModel.softDelete(category.categoryId) },
+                            onSoftDelete = { categoryViewModel.softDelete(category.categoryId) },
                         )
                     }
                 }
@@ -61,9 +98,7 @@ fun ViewCategoryScreen(categoryViewModel: CategoryViewModel = hiltViewModel()) {
 
         CustomFloatingActionButton(
             onClick = { /*shouldShowDialog.value = true*/ },
-            modifier = Modifier
-                .align(Alignment.BottomEnd)
+            modifier = Modifier.align(Alignment.BottomEnd)
         )
     }
 }
-
