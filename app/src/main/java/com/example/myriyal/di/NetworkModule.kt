393e52f18115e5e3d32b325a61f356fcd471e72c
Mohammed <mojazi42@gmail.com>
2025-04-24 12:44:08 +0300

    MR-101 - Adding interceptor

--- app/src/main/java/com/example/myriyal/di/NetworkModule.kt
+++ app/src/main/java/com/example/myriyal/di/NetworkModule.kt
@@ -1,10 +1,12 @@
 package com.example.myriyal.di
 
-import com.example.myriyal.screens.categories.data.api.CategoryApiService
+import androidx.security.crypto.EncryptedSharedPreferences
+import com.example.myriyal.screens.authentication.data.data_sources.AuthInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -12,14 +14,33 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
-    @Provides
-    fun provideBaseUrl() = "https://project-1-admissions3.replit.app"
 
     @Provides
     @Singleton
-    fun provideRetrofit(baseUrl: String): Retrofit =
-        Retrofit.Builder()
-            .baseUrl(baseUrl)
+    fun provideAuthInterceptor(
+        encryptedPrefs: EncryptedSharedPreferences
+    ): AuthInterceptor {
+        return AuthInterceptor(encryptedPrefs)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(
+        authInterceptor: AuthInterceptor
+    ): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl("https://project-1-admissions3.replit.app/")
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+    }
+
 }
