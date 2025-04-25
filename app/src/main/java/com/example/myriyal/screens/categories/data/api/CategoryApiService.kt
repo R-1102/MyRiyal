7a20e08524017645ddad8c9679bcabd1ee8fd1a6
Ahad <ahadsaad45@gmail.com>
2025-04-25 09:32:42 +0300

    Debug-Showing the categories in the UI.

--- app/src/main/java/com/example/myriyal/screens/categories/data/api/CategoryApiService.kt
+++ app/src/main/java/com/example/myriyal/screens/categories/data/api/CategoryApiService.kt
@@ -19,7 +19,6 @@ interface CategoryApiService {
     // From local --> Remote
     @POST ("/categories")
     suspend fun postCategory(
-        @Header ("authorization") token : String = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IjkwOTg1NzhjNDg4MWRjMDVlYmYxOWExNWJhMjJkOGZkMWFiMzRjOGEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vZXhwZW5zZS10cmFja2VyLTc4YjA3IiwiYXVkIjoiZXhwZW5zZS10cmFja2VyLTc4YjA3IiwiYXV0aF90aW1lIjoxNzQ1NTEzMzQzLCJ1c2VyX2lkIjoiUXVGaElyWXlyb1V4RnkwVzBwU0hTbEhzeVJDMiIsInN1YiI6IlF1RmhJcll5cm9VeEZ5MFcwcFNIU2xIc3lSQzIiLCJpYXQiOjE3NDU1MTMzNDMsImV4cCI6MTc0NTUxNjk0MywiZW1haWwiOiJsZWVuMjAyNUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50a",
         @Body category: CategoryDto
     ) : Response<CategoryResponse>
 
